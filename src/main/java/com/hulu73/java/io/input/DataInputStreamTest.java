a7fa92811234b5620c0b334488ddc6739190a43e
lzg <472353391@qq.com>
2018-09-19 23:54:07 +0800

    数据流

--- src/main/java/com/hulu73/java/io/input/DataInputStreamTest.java
+++ src/main/java/com/hulu73/java/io/input/DataInputStreamTest.java
@@ -7,7 +7,7 @@ import java.io.*;
 /**
  * @Auther: liuzhg
  * @Date: 2018/9/19 0019
- * @Description:
+ * @Description:DataInputStream使用FilterInputStream包含的流读取字节，并将字节转换为java的原始类型
  */
 public class DataInputStreamTest {
     public  static void main(String[] args) throws IOException {
